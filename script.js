051adbc0089bf2a0172de22fce5e7eb10e07f897
EduBatageli <edumbat@gmail.com>
2023-05-10 10:56:13 -0300

    arquivos js

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-const text = document.querySelector(".mudar");
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
 
-        const textLoad = () => {
-            setTimeout(() => {
-                text.textContent = "Python";
-            }, 0);
-            setTimeout(() => {
-                text.textContent = "SQL";
-            }, 4000);
-            setTimeout(() => {
-                text.textContent = "Java";
-            }, 8000); //1s = 1000 milliseconds
-        }
+const textArray = ["Python", "MYSQL", "HTML", "CSS", "JS"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
 
-        textLoad();
-        setInterval(textLoad, 12000);
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
