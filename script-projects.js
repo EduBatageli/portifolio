d3421d1ab9641002dde018fdb76d226faef67810
edubatageli <edumbat@gmail.com>
2025-02-07 21:56:21 -0300

    mudando idioma pag projetos

--- script-projects.js
+++ script-projects.js
@@ -8,5 +8,14 @@ const theme = params.get("theme"); // 'dark'
 const html = document.documentElement;
 html.classList.add(theme)
 
-// Exibe no console
-console.log(`Idioma: ${lang}, Tema: ${theme}`);
+
+if(lang === "ptbr") {
+    document.getElementById("titulo_projeto1").innerHTML = "Plataforma de Gerenciamento de Senhas"
+    document.getElementById("desc_projeto1").innerHTML = "Plataforma segura para armazenar, gerar e gerenciar senhas com criptografia."
+
+}
+else {
+    document.getElementById("titulo_projeto1").innerHTML = "Password Management Platform"
+    document.getElementById("desc_projeto1").innerHTML = "Secure platform for storing, generating, and managing passwords with encryption."
+
+}
