fc5b1f86c1b7fa07dc935ea5ad244e6db415ff2f
edubatageli <edumbat@gmail.com>
2025-02-07 22:38:58 -0300

    Arrumando btn voltar

--- script-projects.js
+++ script-projects.js
@@ -12,10 +12,12 @@ html.classList.add(theme)
 if(lang === "ptbr") {
     document.getElementById("titulo_projeto1").innerHTML = "Plataforma de Gerenciamento de Senhas"
     document.getElementById("desc_projeto1").innerHTML = "Plataforma segura para armazenar, gerar e gerenciar senhas com criptografia."
+    document.getElementById("button-back").innerHTML = "Voltar"
 
 }
 else {
     document.getElementById("titulo_projeto1").innerHTML = "Password Management Platform"
     document.getElementById("desc_projeto1").innerHTML = "Secure platform for storing, generating, and managing passwords with encryption."
+    document.getElementById("button-back").innerHTML = "Back"
 
 }
--- script-sumary.js
+++ script-sumary.js
@@ -18,6 +18,7 @@ if (lang === "ptbr") {
     document.getElementById("timeline").innerHTML = "Skyone | 2024 - Presente"
     document.getElementById("job_desc1").innerHTML = "Desenvolvendo integrações de sistemas, implementando melhorias e automatizando processos por meio de agentes de inteligência artificial. Realizando validação de processos e promovendo a melhoria contínua dos processos internos, com foco em eficiência, inovação e otimização operacional."
     document.getElementById("Professional-session2").innerHTML = "Arquiteto de Soluções de Nuvem"
+    document.getElementById("button-back").innerHTML = "Voltar"
     document.getElementById("job-desc2").innerHTML = "Colaborando com a equipe de arquitetura no design de projetos para novos clientes e na execução de upgrades em ambientes de nuvem (Oracle, AWS, Azure) para clientes existentes, garantindo alto desempenho, segurança e escalabilidade das soluções implementadas. Além disso, atuando em conjunto com a equipe de Customer Success para validar ambientes e impulsionar a retenção de clientes, promovendo satisfação e sucesso contínuo em suas operações."
 
 } else {
@@ -30,5 +31,6 @@ if (lang === "ptbr") {
     document.getElementById("timeline").innerHTML = "Skyone | 2024 - Present"
     document.getElementById("job_desc1").innerHTML = "Developing system integrations, implementing improvements, and automating processes through artificial intelligence agents. Conducting process validation and driving continuous improvement of internal processes, with a focus on efficiency, innovation, and operational optimization."
     document.getElementById("Professional-session2").innerHTML = "Cloud Solutions Architech"
+    document.getElementById("button-back").innerHTML = "Back"
     document.getElementById("job-desc2").innerHTML = "Collaborating with the architecture team in designing projects for new clients and executing upgrades in cloud environments (Oracle, AWS, Azure) for existing clients, ensuring high performance, security, and scalability of the implemented solutions. Additionally, working alongside the Customer Success team to validate environments and drive client retention, fostering satisfaction and ongoing success in their operations"
 }
