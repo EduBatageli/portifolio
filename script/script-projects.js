fe9f923981971b4dcd7eb61f07abab0b4c267874
edubatageli <edumbat@gmail.com>
2025-03-25 20:37:35 -0300

    add projeto 1

--- script/script-projects.js
+++ script/script-projects.js
@@ -13,12 +13,16 @@ if(lang === "ptbr") {
     document.getElementById("titulo_projeto1").innerHTML = "Plataforma de Gerenciamento de Senhas"
     document.getElementById("desc_projeto1").innerHTML = "Plataforma segura para armazenar, gerar e gerenciar senhas com criptografia."
     document.getElementById("button-back").innerHTML = "Voltar"
+    document.getElementById("titulo_projeto1").innerHTML = "Gerador de Receitas com IA"
+    document.getElementById("desc_projeto1").innerHTML = "Agente inteligente que gera receitas personalizadas com base nos ingredientes fornecidos."
 
 }
 else {
     document.getElementById("titulo_projeto1").innerHTML = "Password Management Platform"
     document.getElementById("desc_projeto1").innerHTML = "Secure platform for storing, generating, and managing passwords with encryption."
     document.getElementById("button-back").innerHTML = "Back"
+    document.getElementById("titulo_projeto1").innerHTML = "AI Recipe Generator"
+    document.getElementById("desc_projeto1").innerHTML = "Intelligent agent that generates personalized recipes based on the ingredients provided."
 
 }
 
